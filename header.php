4e365bd87c2e34840a5e06e6b8d75918d06df402
rezaalamsyah <53548427+rezaalamsyah@users.noreply.github.com>
2020-03-25 14:34:47 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="http://localhost/siperpus/aset/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://localhost/siperpus/aset/fontawesome/css/all.min.css">
+
+    <title>SIPERPUS</title>
+</head>
+<body>
+
+
+   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#"><i class="fas fa-book-reader text-white mr-2"> SIPERPUS |Hai, Reza</i></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link" href="http://localhost/siperpus/index.php">Dashboard</a>
+      <a class="nav-item nav-link" href="http://localhost/siperpus/buku/index.php">Buku</a>
+      <a class="nav-item nav-link" href="http://localhost/siperpus/anggota/index.php">Anggota</a>
+      <a class="nav-item nav-link" href="http://localhost/siperpus/transaksi/index.php">Peminjaman</a>
+      <a class="nav-item nav-link" href="#">Logout</a>
+
+        </div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
